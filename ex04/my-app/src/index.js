7c7ae1b35b399d16d22bf65ec99cf70feb562da5
ABATarik <abaspahic.tarik@outlook.com>
2021-03-27 10:14:16 +0100

    04

---
+++ ex04/my-app/src/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+       <div className="App">
+         <h1>Hello, Padavans!</h1>
+       </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
